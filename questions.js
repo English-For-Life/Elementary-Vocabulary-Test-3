86e443fe2c8663a12d9afbac2aeb830f185e255a
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-07-07 22:19:06 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"It is cold in England when a north wind ______ .",
+        options:["blows", "goes", "covers", "passes"],
+        answer: 0
+    },
+    {
+        q:"It is warm and pleasant when the sun ______ .",
+        options:["comes", "shines", "lights", "pours"],
+        answer: 1
+    },
+    {
+        q:"When it rains very heavily, it ______ .",
+        options:["waters", "snows", "pours", "freezes"],
+        answer: 2
+    },
+    {
+        q:"My friends say I like to ______ money.",
+        options:["gain", "find", "see", "spend"],
+        answer: 3
+    },
+    {
+        q:"They have everything you ______ for your house.",
+        options:["need", "adore", "like", "have"],
+        answer: 0
+    },
+    {
+        q:"You can ______ everything you need at this supermarket.",
+        options:["use", "buy", "sell", "lend"],
+        answer: 1
+    },
+    {
+        q:"If you can't ______ your watch, buy a new one.",
+        options:["get", "do", "find", "sell"],
+        answer: 2
+    },
+    {
+        q:"I am ______ my glasses. Where can they be?",
+        options:["using", "seeing", "finding", "looking for"],
+        answer: 3
+    },
+    {
+        q:"My father ______ shoes in a store.",
+        options:["sells", "makes", "wears", "gives"],
+        answer: 0
+    },
+    {
+        q:"I forget to ______ for goods that I buy every time.",
+        options:["buy", "pay", "sell", "give"],
+        answer: 1
+    }
+
+
+
+]
